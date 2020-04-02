f9b16746129a924760a5a229bbd2a4f1fa8f1aa8
Luis Martín Canaval Sánchez <uno@canaval.org>
2020-04-02 17:29:54 +0200

    hanoi

---
+++ semana02/hanoi.cpp
@@ -0,0 +1,19 @@
+// hanoi
+//
+
+#include <iostream>
+
+using namespace std;
+
+void hanoi(int n, char a, char b, char c) {
+    if (n > 0) {
+        hanoi(n - 1, a, c, b);
+        cout << "Mover disco " << n << " de " << a << " a " << b << endl;
+        hanoi(n - 1, c, b, a);
+    }
+}
+
+int main() {
+    hanoi(4, 'A', 'B', 'C');
+    return 0;
+}
